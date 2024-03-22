07fd9dbff17773e2c12d4cb1f439e8d50bb53ffc
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-22 23:10:20 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,7 +1,7 @@
 # متطلبات التنصيب #
 OWNER = ["K_O_W_4", "IIUll_l"]
 OWNER_NAME = "ْ𓆩⧛ َ 𝘼َِ𝙁َِ𝙍َِ𝙊َِ𝙊َِ𝙏َِ𝙊ِ ┇ عـ๋͜‏ـۂفــ͡ـࢪتوُ ⧚𓆪"
-BOT_TOKEN = "6477407510:AAHdX4kXIH3CILzRFFhhBgvKgrOkruYXuqc"
+BOT_TOKEN = "6477407510:AAH7qtd4tabFhx7o5wzwsB-mwUd_TqIozFk"
 DATABASE = "mongodb+srv://bot_vambir:Al2552001@cluster0.heabj.mongodb.net/vambir_bot?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/UI_VM"
 GROUP = "https://t.me/YR_HX"
